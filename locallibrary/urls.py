25bd0547bf9f8cba204013209acba2935099f998
Andrii Venher <vengerandr@gmail.com>
2026-03-27 09:29:12 +0100

    Set up Django project

---
+++ locallibrary/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # Forward all 'catalog/' URLs to the app's URL configuration
+    path('catalog/', include('catalog.urls')),
+    # Redirect the root URL to the catalog app
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
