cc38c956f54af85e9e9761df7128e431840b60e2
Andrii Venher <vengerandr@gmail.com>
2026-03-27 09:50:33 +0100

    Set up home page

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,10 @@
+from datetime import date
+import uuid
+
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
-import uuid 
+
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -9,12 +13,14 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+
 class Language(models.Model):
     """Model representing a Language (e.g. English, French, Japanese, etc.)"""
     name = models.CharField(max_length=200, help_text="Enter the book's natural language")
 
     def __str__(self):
         return self.name
+
 
 class Author(models.Model):
     """Model representing an author."""
@@ -26,8 +32,13 @@ class Author(models.Model):
     class Meta:
         ordering = ['last_name', 'first_name']
 
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
+
 
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
@@ -36,17 +47,38 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='13 Character ISBN number')
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
-    language = models.ManyToManyField(Language, help_text='Select a language for this book')
+    language = models.ForeignKey(
+        Language,
+        on_delete=models.SET_NULL,
+        null=True,
+        help_text='Select a language for this book',
+    )
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
+
+
 class BookInstance(models.Model):
     """Model representing a specific copy of a book."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this copy')
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        help_text='Unique ID for this particular book across whole library',
+    )
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -65,6 +97,13 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (('can_mark_returned', 'Set book as returned'),)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     def __str__(self):
         return f'{self.id} ({self.book.title})'
--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
-from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+from .models import Author, Book, BookInstance
+
 
 def index(request):
     """View function for home page of site."""
-    # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    
-    # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    
+    num_authors = Author.objects.count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
     }
 
-    # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    queryset = Author.objects.all()
